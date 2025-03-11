ef5fca34b948b588fc07df2d8c48a290d3ad7b92
jason lui <jason.lui@r2c2.ai>
2025-03-12 01:58:08 +0800

    add allow paths

--- ui.py
+++ ui.py
@@ -248,4 +248,4 @@ def create_gradio_interface():
 
 if __name__ == "__main__":
     demo = create_gradio_interface()
-    demo.launch(server_port=8080)
+    demo.launch(server_port=8080, allowed_paths=['/','/workspace/unsloth-gradio-ui'])
