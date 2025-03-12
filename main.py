eb85a825cba6af93871e5c57dfacc5a2ac4130e1
jason lui <jason.lui@r2c2.ai>
2025-03-12 09:17:49 +0800

    change on download, edit main for fastapi

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from ui import create_gradio_interface
 
-if __name__ == "__main__":
-    demo = create_gradio_interface()
-    demo.launch(server_port=8080)
+# if __name__ == "__main__":
+#     demo = create_gradio_interface()
+#     demo.launch(server_port=8080)
+
+from fastapi import FastAPI
+import gradio as gr
+
+app = FastAPI()
+demo = create_gradio_interface()
+app = gr.mount_gradio_app(app, demo, path="/")
